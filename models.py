d32f6507e969f79dd7d661402f3dba7245393a2c
Joey Turczak <joeyturczak@gmail.com>
2017-09-17 10:33:18 -0700

    Create catalog item database table

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
+    name = Column(String(80), nullable = False)
 
     @property
     def serialize(self):
@@ -18,3 +18,24 @@ class Category(Base):
             'id': self.id,
             'name': self.name
         }
+
+class CatalogItem(Base):
+
+    __tablename__ = 'catalog_item'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description
+        }
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
