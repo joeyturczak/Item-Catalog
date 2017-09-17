2aaf97d37f3439f2325b30b3bcf935eda0c21f1c
Joey Turczak <joeyturczak@gmail.com>
2017-09-17 10:39:17 -0700

    Add database connection to app

--- application.py
+++ application.py
@@ -1,6 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, CatalogItem
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 8000)
