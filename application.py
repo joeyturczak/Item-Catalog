69b28c54a98218b5e7209b31ad8ad1e3fa917bd5
Joey Turczak <joeyturczak@gmail.com>
2017-09-17 12:02:49 -0700

    Add app route for main page

--- application.py
+++ application.py
@@ -5,10 +5,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Category, CatalogItem
 
+# Connect to database and create database session
 engine = create_engine('sqlite:///itemcatalog.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
+
+# Show main catalog page
+@app.route('/')
+@app.route('/catalog/')
+def showCatalog():
+    categories = session.query(Category).order_by(asc(Category.name))
+    return render_template('catalog.html', categories=categories)
 
 if __name__ == '__main__':
     app.debug = True
