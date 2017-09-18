26eaea5dffa7d916690d511133e5ba6ffb01e600
Joey Turczak <joeyturczak@gmail.com>
2017-09-17 17:23:51 -0700

    Add route to edit item page

--- application.py
+++ application.py
@@ -1,14 +1,9 @@
-from flask import Flask, url_for, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, request
 app = Flask(__name__)
 
-from sqlalchemy import create_engine, asc, desc
+from sqlalchemy import create_engine, asc, desc, and_
 from sqlalchemy.orm import sessionmaker
 from models import Base, Category, CatalogItem
-
-#import sys
-#import codecs
-#sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-#sys.stderr = codecs.getwriter('utf8')(sys.stderr)
 
 # Connect to database and create database session
 engine = create_engine('sqlite:///itemcatalog.db')
@@ -34,8 +29,38 @@ def showCategory(category_name):
 
 @app.route('/catalog/<string:category_name>/<string:item_name>')
 def showItem(category_name, item_name):
-    item = session.query(CatalogItem).filter_by(name=item_name).one()
+    item = session.query(CatalogItem).filter(and_(CatalogItem.name==item_name, CatalogItem.category_name==category_name)).one()
     return render_template('item.html', item=item)
+
+@app.route('/catalog/new', methods=['GET', 'POST'])
+def newItem():
+    if request.method == 'POST':
+        newItem = CatalogItem(name=request.form['name'], description=request.form['description'], category_name=request.form['category'])
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('showCatalog'))
+    else:
+        categories = session.query(Category).order_by(asc(Category.name))
+        return render_template('new_item.html', categories=categories)
+
+@app.route('/catalog/<string:category_name>/<string:item_name>/edit', methods=['GET', 'POST'])
+def editItem(category_name, item_name):
+    itemToEdit = session.query(CatalogItem).filter(and_(CatalogItem.name==item_name, CatalogItem.category_name==category_name)).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            itemToEdit.name = request.form['name']
+            itemToEdit.description = request.form['description']
+            itemToEdit.category_name = request.form['category']
+            session.add(itemToEdit)
+            session.commit()
+            return redirect(url_for('showCategory', category_name=category_name))
+    else:
+        categories = session.query(Category).order_by(asc(Category.name))
+        return render_template('edit_item.html', item=itemToEdit, categories=categories)
+
+@app.route('/catalog/<string:item_name>/edit', methods=['GET', 'POST'])
+def deleteItem():
+    return "Delete Item"
 
 if __name__ == '__main__':
     app.debug = True
--- fill_database.py
+++ fill_database.py
@@ -13,11 +13,11 @@ session.add(category)
 session.commit()
 
 item1 = CatalogItem(name="Sofa", description="A piece of furniture for seating"
-                    "three or more people in the form of a bench", category=category)
+                    "three or more people in the form of a bench.", category=category)
 session.add(item1)
 session.commit()
 
-item2 = CatalogItem(name="Coffee Table", description="A style of long, low table which is designed to be placed in front of a sofa",
+item2 = CatalogItem(name="Coffee Table", description="A style of long, low table which is designed to be placed in front of a sofa.",
                     category=category)
 session.add(item2)
 session.commit()
@@ -37,5 +37,8 @@ session.commit()
 category = Category(name = "Bathroom")
 session.add(category)
 session.commit()
+
+item1 = CatalogItem(name="Sink", description="A bowl-shaped fixture used for washing hands, dishwashing, and other purposes.",
+                    category=category)
 
 print "Added catalog items!"
