1347a5a9de2ad8c4bd8442ddc47f5489d70b8e4f
Joey Turczak <joeyturczak@gmail.com>
2017-09-24 14:41:23 -0700

    Add breakpoint for smaller displays

--- application.py
+++ application.py
@@ -19,11 +19,11 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 # Create anti-forgery state token
-# @app.route('/login')
-# def showLogin():
-#     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
-#     login_session['state'] = state
-#     return render_template('login.html', STATE=state)
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(32))
+    login_session['state'] = state
+    return render_template('login.html', STATE=state)
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
